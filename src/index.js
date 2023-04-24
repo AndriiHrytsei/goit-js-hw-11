46136f99cb2fd69bd22f5e7e2d1092588eeabd24
Andrii Hrytsei <andrii.hrytsei@gmail.com>
2023-04-24 11:54:23 +0300

    add styles

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import * as mainJS from "./js/main.js"
 
 mainJS.searchForm.addEventListener("submit", (e)=>{
     e.preventDefault()
-    mainJS.fetchImages(`https://pixabay.com/api/?key=35594812-0318ae570b601c4a3427f19fb&q=${mainJS.searchInput.value.toLowerCase()}&image_type=photo&orientation=horizontal&safesearch=true`)
+    mainJS.fetchImages(`https://pixabay.com/api/?key=35594812-0318ae570b601c4a3427f19fb&q=${mainJS.searchInput.value.toLowerCase()}&image_type=photo&orientation=horizontal&safesearch=true&per_page=100`)
     .then(images => {
-        mainJS.gallery.replaceChildren("")
+        mainJS.gallery.replaceChildren([])
         mainJS.renderImages(images["hits"])
     })
 })
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 export const gallery = document.querySelector(".gallery")
 export const searchInput = document.querySelector(".search-input")
 export const searchForm = document.querySelector(".search-form")
+
 export async function fetchImages (api) {
     try{
         const images = await fetch(api)
@@ -13,22 +14,26 @@ export async function fetchImages (api) {
 
 export function renderImages(photos) {
     photos.forEach(photo => {
-        gallery.insertAdjacentHTML("afterbegin", 
+        gallery.insertAdjacentHTML("beforeend", 
         `<div class="photo-card">
             <img src="${photo.webformatURL}" alt="${photo.tags}" loading="lazy" />
             <div class="info">
-                <p class="info-item">
-                    <b>Likes ${photo.likes}</b>
-                </p>
-                <p class="info-item">
-                    <b>Views ${photo.views}</b>
-                </p>
-                <p class="info-item">
-                    <b>Comments ${photo.comments}</b>
-                </p>
-                <p class="info-item">
-                    <b>Downloads ${photo.downloads}</b>
-                </p>
+                <div class="info-item">
+                    <p>Likes</p>
+                    <p>${photo.likes}</p>
+                </div>
+                <div class="info-item">
+                    <p>Views</p>
+                    <p>${photo.views}</p>
+                </div>
+                <div class="info-item">
+                    <p>Comments</p>
+                    <p>${photo.comments}</p>
+                </div>
+                <div class="info-item">
+                    <p>Downloads</p>
+                    <p>${photo.downloads}</p>
+                </div>
             </div>
         </div>
         `)
